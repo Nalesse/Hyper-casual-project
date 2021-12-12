3aa76d4045f9d29a695501b28cac77d223141f31
Nalesse <nalesse@student.fitchburgstate.edu>
2021-12-11 20:16:47 -0500

    Transition from far cam to normal cam
    At the start the game the camera will be pulled back so you can see the police officer, then after a few seconds the camera will snap to the player.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,15 +7,50 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform player;
 
+    [SerializeField] private float cameraSpeed;
+
+    [SerializeField] private Vector3 endOffset;
+    [SerializeField] private float cameraLerpDelay;
+   
+
     private Vector3 offset;
+    private bool doLerp;
+
+
 
     private void Awake()
     {
         offset = transform.position - player.transform.position;
     }
 
+    private void Start()
+    {
+        StartCoroutine(CameraLerp());
+    }
+
     private void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        StartCoroutine(CameraLerp());
+
+        if (!doLerp)
+        {
+            transform.position = player.transform.position + offset;
+        }
+        else
+        {
+            var startPos = player.transform.position + offset;
+            var endPos = player.transform.position + endOffset;
+            offset.z = endOffset.z;
+
+            transform.position = Vector3.MoveTowards(startPos, endPos, cameraSpeed * Time.deltaTime);
+        }
+        
+    }
+
+    private IEnumerator CameraLerp()
+    {
+        yield return new WaitForSeconds(cameraLerpDelay);
+        doLerp = true;
+        
     }
 }
